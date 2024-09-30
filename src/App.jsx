d2d0923873ebc941e93e8c877aaf0c10befbb49a
Bentanick <kennyjamqw@gmail.com>
2024-10-01 02:36:18 +0700

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import { useDispatch, useSelector } from "react-redux";
-import "./App.css";
-import { getLogin } from "./stores/authSlice";
-import Home from "@/pages/home";
+import { RouterProvider } from "react-router-dom";
+
+import './App.css';
+import { router } from "./configs/routes";
 
 function App() {
-  const authState = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-
-  const handleClick = () => {
-    dispatch(getLogin());
-  };
   return (
-    <>
-      <div className="bg-red-500 text-red-500">
-        Hello
-        <button onClick={handleClick}>Nhan</button>
-        <Home />
-      </div>
-    </>
-  );
+    <RouterProvider router={router} />
+  )
 }
 
-export default App;
+export default App
